1fe47cc0e75dcead990c91041e6908d72bda6234
niuniu <1240495040@qq.com>
2021-10-19 18:02:07 +0800

    更新了login.html的注册用户

--- assets/js/login.js
+++ assets/js/login.js
@@ -15,6 +15,31 @@ $(function(){
     var form = layui.form
     // 通过 form.verify()函数自定义校验规则
     form.verify({
-        pwd:[/^[\S]{6,12}$/,'密码必须6到12位，且不能出现空格']
+        pwd:[/^[\S]{6,12}$/,'密码必须6到12位，且不能出现空格'],
+
+        // 校验两次密码是否一致的规则
+        repwd:function(value){
+            // 通过形参拿到密码中的内容
+            // 再通过判断，若两者不一致则return出提示消息
+            var pwd = $('.reg-box [name=password]').val()
+            if(pwd !== value) {
+                return alert('两次密码不一致,请重新输入！')
+            }
+        }
+    })
+
+    // 监听注册表单的提交事件
+    $('#form_reg').on('submit',function(e){
+        // 阻止默认行为
+        e.preventDefault();
+
+        // 发起ajax的post请求
+        $.post('http://ajax.frontend.itheima.net/api/reguser', { username: $('#form_reg [name=username]').val(), password: $('#form_reg [name=password]').val() }, 
+        function(res){
+            if(res.status!==0) {
+                    return layui.msg(res.massage);
+            }
+            layui.msg('注册成功，请登录！')
+        })
     })
 })
