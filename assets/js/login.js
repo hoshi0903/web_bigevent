a41c77b06780ecab2c92bf51b702caefad99a249
benben <1240495040@qq.com>
2021-10-20 23:30:08 +0800

    更新了登录用户功能

--- assets/js/login.js
+++ assets/js/login.js
@@ -13,6 +13,8 @@ $(function(){
 
     // 从layui中获取form对象
     var form = layui.form
+
+    var layer = layui.layer
     // 通过 form.verify()函数自定义校验规则
     form.verify({
         pwd:[/^[\S]{6,12}$/,'密码必须6到12位，且不能出现空格'],
@@ -41,9 +43,34 @@ $(function(){
         $.post("http://api-breakingnews-web.itheima.net/api/reguser", data, 
         function(res){
             if(res.status !== 0) {
-                    return console.log(res.message);
+                    return layer.msg(res.message);
             }
-            console.log('注册成功，请登录！');
+            layer.msg('注册成功，请登录！');
+            // 模拟人的点击行为 点击登录 跳到登录窗口
+            $('#link_login').click();
+        })
+    })
+
+    // 监听登录表单的提交事件
+    $('#form_login').on('submit',function(e){
+        // 阻止默认行为
+        e.preventDefault();
+        $.ajax({
+            url:'http://api-breakingnews-web.itheima.net/api/login',
+            method:'POST',
+            // 快速获取表单中的数据
+            data:$(this).serialize(),
+            success:function(res){
+                if(res.status !== 0) {
+                    return layer.msg('登录失败！')
+                }
+                layer.msg('登录成功！')
+                console.log(res.token);
+                // Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NDA0ODAsInVzZXJuYW1lIjoi6IOW57qiIiwicGFzc3dvcmQiOiIiLCJuaWNrbmFtZSI6IiIsImVtYWlsIjoiIiwidXNlcl9waWMiOiIiLCJpYXQiOjE2MzQ3NDM3MjcsImV4cCI6MTYzNDc3OTcyN30.T31Jczwq33CvPsXEUgr4MlJvU_IXMDakGRYRgHv2xKU
+
+                // 跳转到后台主页
+                location.href = '/大事件管理后台/web_bigevent/index.html'
+            }
         })
     })
 })
