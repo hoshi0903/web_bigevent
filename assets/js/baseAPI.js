f7f385987db000dacb8655a2ca00eaf409218081
benben <1240495040@qq.com>
2021-10-29 19:51:19 +0800

    完成了主页的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,8 +1,26 @@
 // 每次调用$.gei()函数或者是$.post()函数，$.ajax()函数等之前都会调用$.ajaxPrefilter这个函数
 // 在这个函数中，可以拿到我们给Ajax()配置的对象
 $.ajaxPrefilter(function(options){
-
+    // 在发起真正的Ajax请求之前，同意拼接请求的根路径
     options.url = 'http://api-breakingnews-web.itheima.net'+ options.url;
-    // http://api-breakingnews-web.itheima.net/api/reguser
+    
+    // 统一为有权限的接口，设置headers请求头
+    // 先判断url是否有my(有权限的接口)
+    if(options.url.indexOf('/my/') !== -1){
+        options.headers = { Authorization: localStorage.getItem('token') ||''}
+        console.log(options.headers)
+    }
+
+    // 全局统一挂载 complete 回调函数
+     // 无论成功还是失败，最终都会调用complete 回调函数
+    options.complete = function (res) {
+        //    在complete回调函数中，可以使用res.reponseJSON 拿到的服务器响应回来的数据
+        if(res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！'){
+            // 1、强制清空token
+            localStorage.removeItem('token')
+            // 2、强制跳转到登录页面
+            location.href = '/web_bigevent/login.html'
+            }
+    }
 
 }) 
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,20 @@
 $(function() {
     // 调用函数获取用户信息
     getUserInfo()
+
+    var layer = layui.layer
+    // 点击退出按钮 实现退出功能
+    $('#btnLogout').on('click',function(){
+        // 提示用户是否确认退出
+        layer.confirm('确定退出登录？',{icon: 3, title:'提示'},function (index) {
+            // 1、清楚本地存储中的token
+           localStorage.removeItem('token')
+            //2、重新跳转到登录页面
+            location.href = '/web_bigevent/login.html'    
+            // 关闭confirm 询问框
+            layer.close(index)
+        })
+    })
 })
 
 // 获取用户信息
@@ -10,17 +24,41 @@ function getUserInfo() {
         url: '/my/userinfo',
         // headers就是请求头的配置对象
         // /my开头的请求路径必须携带Authorization身份字段认证，要求为 localStorage.getItem('token')
-        headers: {
-            Authorization: localStorage.getItem('token')
-        },
+        // headers: {
+        //     Authorization: localStorage.getItem('token') ||''},
             success: function(res) {
                 if(res.status !== 0) {
                     console.log(res);
                     return layui.layer.msg('获取用户信息失败！')
                 } 
                 // 调用renderAvatar 渲染用户的头像
-                // renderAvatar()
                 console.log(res);
+                renderAvatar(res.data)
         }
     })
 }
+
+// 渲染用户头像
+function renderAvatar(user) {
+
+    // 1、获取用户的名称
+    var name = user.nickname || user.username
+    // 2、设置欢迎的文本
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+
+    // 按需渲染用户的头像
+    if(user.user_pic !== null) {
+        // 渲染用户的头像
+        $('.layui-nav-img').attr('src',user.user_pic).show()
+        $('.text-avatar').hide()
+    } else {
+
+        // 渲染文本头像
+        $('.layui-nav-img').hide()
+        var first = name[0].toUpperCase()//获取用户名第一个字符
+        console.log(first);
+        $('.text-avatar').html(first).show()
+    }
+}
+
+
