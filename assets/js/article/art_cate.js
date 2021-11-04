32f4af944fc5b36facdde3bbcf3b423a9ddf1c36
niuniu <1240495040@qq.com>
2021-11-04 17:49:59 +0800

    更新了文章删除功能

--- assets/js/article/art_cate.js
+++ assets/js/article/art_cate.js
@@ -16,6 +16,7 @@ $(function(){
                 // layer.msg('获取文章列表成功！');
                 var htmlStr = template('tpl-table',res);
                 $('tbody').html(htmlStr)
+                console.log(res);
             }
         })
     }
@@ -52,6 +53,31 @@ $(function(){
                 layer.close(indexAdd);
             }
         })
+    })
+
+    // 通过代理的形式，为tpl-table表单绑定点击事件
+    $('tbody').on('click','.btn-delete',function(){
+        // $(selector).attr(attribute) attribute:规定要获取其值的属性。
+        var id = $(this).attr("data-id");
+        console.log('OK!');
+        console.log(id);
+
+        // 提示框
+        layer.confirm('确认删除？', {icon: 3, title:'提示'}, function(index){
+            
+            $.ajax({
+                method:'GET',
+                url: '/my/article/deletecate/' + id,
+                success: function(res) {
+                    if(res.status !== 0) {
+                        return layer.msg('删除分类失败！')
+                    }
+                    layer.msg('删除分类成功！')
+                    layer.close(index);
+                    initArtCateList()
+                }
+            })
+          })
     })
 
 })
